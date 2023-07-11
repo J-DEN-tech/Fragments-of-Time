c4aae62db502a7229dce7b71d7fe6b32a94aefc2
Ben-Lever <1026824@student.sae.edu.au>
2023-07-11 16:04:56 +0800

    Leaving fungus for now, remade room with the new assets, made window and chest clickable

---
+++ FragmentsOfTime/Assets/Scripts/ToyChestScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToyChestScript : MonoBehaviour
+{
+    public List<Sprite> ToyChestSprite; //All backgrounds can be placed in here
+    public SpriteRenderer image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ToyChestOpen()
+    {
+        image.sprite = ToyChestSprite[1];
+    }
+}
