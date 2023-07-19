f7c4e9a2eae0bd21bccfe074957575e89d3ee237
Kelecia <1028416@student.sae.edu.au>
2023-07-19 19:16:35 +0800

    tut done, although prob didnt need to be own scene

--- FragmentsOfTime/Assets/Scripts/Drag.cs
+++ FragmentsOfTime/Assets/Scripts/Drag.cs
@@ -5,15 +5,43 @@ using UnityEngine;
 public class Drag : MonoBehaviour
 {
     Vector2 difference = Vector2.zero;
-    //_spriteRenderer = GetComponent<SpriteRendere>
+
+    private Color[] colors = { Color.red, Color.green, Color.blue }; // Add any colors you want here
+    private int currentColorIndex = 0;
+    private SpriteRenderer spriteRenderer;
+
+    private AudioSource audioSource;
+    public AudioClip clickSound;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        audioSource = GetComponent<AudioSource>();
+    }
 
     private void OnMouseDown()
     {
         difference = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - (Vector2)transform.position;
+        ChangeColorOnClick();
+        PlayClickSound();
     }
 
     private void OnMouseDrag()
     {
         transform.position = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - difference;
+    }
+
+    private void ChangeColorOnClick()
+    {
+        currentColorIndex = (currentColorIndex + 1) % colors.Length;
+        spriteRenderer.color = colors[currentColorIndex];
+    }
+
+    private void PlayClickSound()
+    {
+        if (clickSound != null && audioSource != null)
+        {
+            audioSource.PlayOneShot(clickSound);
+        }
     }
 }
--- FragmentsOfTime/Assets/Scripts/MenuManager.cs
+++ FragmentsOfTime/Assets/Scripts/MenuManager.cs
@@ -23,5 +23,13 @@ public class MenuManager : MonoBehaviour
         soundPlayer.Play();
     }
 
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 0);
+    }
 
+    public void Tutorial()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 10);
+    }
 }
